e1d8a00ca0effa9014a80d3d026e6664b41c776a
Dominik Brugger <dominikbrugger@posteo.de>
2017-11-26 23:30:53 +0100

    Fixed coveralls path.

--- build_circleci.sh
+++ build_circleci.sh
@@ -6,9 +6,12 @@ git submodule update --init --recursive
 # Build thirdparty
 ./build_thirdparty.sh
 
-for PY_VER in `ls -d /opt/python/cp* | grep -E "cp2|3.*"`; do 
+mkdir test-results
+mkdir cover
+for PY_VER in `ls -d /opt/python/cp* | grep -E "cp34|35|36.*"`; do 
   PYTHON="$PY_VER/bin/python"
   PIP="$PY_VER/bin/pip"
+  COVERALLS="$PY_VER/bin/coveralls"
 
   echo "Building for python version `$PYTHON --version`"
 
@@ -22,10 +25,8 @@ for PY_VER in `ls -d /opt/python/cp* | grep -E "cp2|3.*"`; do
   cp -r dist /tmp
 
   # Run tests
-  mkdir test-results
-  mkdir cover
   $PYTHON setup.py nosetests
-  COVERALLS_REPO_TOKEN=VcjHJa4uHN87FO2LAn1Sg5yMH0zB4EXj0 coveralls
+  COVERALLS_REPO_TOKEN=VcjHJa4uHN87FO2LAn1Sg5yMH0zB4EXj0 $COVERALLS
   mkdir /tmp/$PYTHON_VERSION
   cp -r test-results /tmp/$PYTHON_VERSION
   cp -r cover /tmp/$PYTHON_VERSION
