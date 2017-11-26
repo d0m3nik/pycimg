f27809e1b0ee0fd06588db6c35a82d215a0a33f1
Dominik Brugger <dominikbrugger@posteo.de>
2017-11-26 08:50:37 +0100

    Fixed yasm path

--- build.bat
+++ build.bat
@@ -16,7 +16,7 @@ popd
 pushd .\thirdparty\libjpeg-turbo
 md build
 pushd build
-cmake -g "Visual Studio 14 2015 Win64" -A x64 -DCMAKE_C_FLAGS="/MD" -DCMAKE_BUILD_TYPE=Release .. -DNASM="C:\Program Files\yasm\yasm-1.3.0-win64.exe"
+cmake -g "Visual Studio 14 2015 Win64" -A x64 -DCMAKE_C_FLAGS="/MD" -DCMAKE_BUILD_TYPE=Release .. -DNASM="C:\yasm\yasm.exe"
 msbuild libjpeg-turbo.sln /p:Configuration=Release /p:Platform="x64"
 copy jconfig.h ..
 popd
