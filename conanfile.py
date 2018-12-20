941334c2e175d731aa12e33bb3857271f3a57768
Dominik Brugger <dominikbrugger@posteo.de>
2018-12-21 00:49:13 +0100

    Use normal jpeg package for Linux 32 bit.

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile
+
+class PyCimgConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "json"
+
+    def requirements(self):
+        self.requires('libtiff/4.0.9@bincrafters/stable')
+        self.requires('libpng/1.6.34@bincrafters/stable')
+        if self.settings.os == "Linux" and self.settings.arch == "x86":
+            self.requires('libjpeg/9c@bincrafters/stable')
+        else:
+            self.requires('libjpeg-turbo/1.5.2@bincrafters/stable')
