2fc4f00a9f721759b77d373021bc9ca140bb247f
Dominik <dominikbrugger@posteo.de>
2026-05-08 22:53:42 +0200

    fix: Update libtiff, libjpeg versions.

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class PyCimgConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
 
     def requirements(self):
-        self.requires('libtiff/4.4.0')
+        self.requires('libtiff/4.7.1')
         self.requires('libpng/1.6.58')
-        self.requires('libjpeg/9e')
+        self.requires('libjpeg/9f')
         self.requires('zlib/1.3.1')
