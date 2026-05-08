ce59664c3b5b59a2319a7cfad762f02899539131
Dominik <dominikbrugger@posteo.de>
2026-05-08 20:31:21 +0200

    fix: Use pybind11 cmake integration.

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,9 @@ from conan import ConanFile
 
 class PyCimgConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "VirtualBuildEnv", "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
 
     def requirements(self):
         self.requires('libtiff/4.4.0')
-        self.requires('libpng/1.6.38')
+        self.requires('libpng/1.6.58')
         self.requires('libjpeg/9e')
